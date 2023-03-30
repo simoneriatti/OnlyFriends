134f709b77e559d4d1434196dbdd88ef0042775e
Elenadc96 <helena.dicicco@gmail.com>
2023-03-30 16:17:36 +0200

    commit getpost

--- only_friends/lib/screen/home_page.dart
+++ only_friends/lib/screen/home_page.dart
@@ -1,13 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:only_friends/widget/bottom_naviagtion_bar.dart';
 import 'package:only_friends/widget/post_card.dart';
+import '../model/postModel.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
 
   final user = FirebaseAuth.instance.currentUser!;
+
+    Future<List<PostModel>> getAllPost() async {
+    final snapshot =
+        await FirebaseFirestore.instance.collection('Utente').get();
+    final postList =
+        snapshot.docs.map((e) => PostModel.fromSnapshot(e)).toList();
+    return postList;
+  }
 
   @override
   Widget build(BuildContext context) {
